aba348699c11308db5bf359f59e2d07534691618
rupesh-kaneki88 <rupeshchavan048@gmail.com>
2025-04-08 13:38:44 +0530

    commented unused code.

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,19 +1,19 @@
-import mongoose, {Schema,Document} from "mongoose";
+// import mongoose, {Schema,Document} from "mongoose";
 
-export interface Message extends Document{
-    content: string;
-    createdAt: Date;
-}
+// export interface Message extends Document{
+//     content: string;
+//     createdAt: Date;
+// }
 
 
-const MessageSchema:Schema<Message>=new Schema({
-content:{
-type:String,
-required:true
-},
-createdAt:{
-    type:Date,
-    required:true,
-    default:Date.now
-}
-})
+// const MessageSchema:Schema<Message>=new Schema({
+// content:{
+// type:String,
+// required:true
+// },
+// createdAt:{
+//     type:Date,
+//     required:true,
+//     default:Date.now
+// }
+// })
