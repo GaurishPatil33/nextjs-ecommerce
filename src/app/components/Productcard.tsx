bc3e11e54815630069abcc34bcaed5edbc97f327
gaurish <33gaurishpatil@gmail.com>
2025-04-08 18:40:16 +0530

    updated product page

--- src/app/components/Productcard.tsx
+++ src/app/components/Productcard.tsx
@@ -5,17 +5,21 @@ import React from "react";
 const Productcard = ({ product }) => {
   return (
     <Link href={`/product/${product.id}`} className=" block group ">
-
       <div className="m-1 p-4 h-full overflow-hidden rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200">
-        <img
-          src={product.thumbnail}
-          alt={product.title}
-          className="w-full h-40 object-cover rounded-md"
-        />
+        <div className="relative w-full h-40">
+          <Image
+            src={product.thumbnail}
+            alt={product.title}
+            fill
+            className=" object-cover rounded-md"
+          />
+        </div>
         <h2 className="text-lg font-medium truncate">{product.title}</h2>
         <div className="flex items-center justify-between text-black">
-          <p>{Math.round(product.price)} ₹</p>
-          <p className=" text-sm text-green-500">{product.discountPercentage} % Off</p>
+          <p className="text-base font-semibold">{Math.round(product.price)} ₹</p>
+          <p className=" text-sm  text-green-500">
+            {product.discountPercentage} % Off
+          </p>
         </div>
       </div>
     </Link>
