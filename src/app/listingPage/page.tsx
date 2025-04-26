d44df0a1f0c5be68e9dd4bd7f91a6f1cef9e2789
gaurish <33gaurishpatil@gmail.com>
2025-04-26 15:34:47 +0530

    updated homepage

--- src/app/listingPage/page.tsx
+++ src/app/listingPage/page.tsx
@@ -7,38 +7,33 @@ import Skeleton from "@/components/Skeleton";
 import Productlist from "@/components/Productlist";
 import Filter from "@/components/Filter";
 
-const ListingPage = () =>
-  // {
-  //   searchParams,
-  //   category,
-  // }: {
-  //   searchParams?: string;
-  //   category?: string;
-  // }
-  {
-    const router = useSearchParams();
-    const query = router.get("search");
-    const category = router.get("cat");
+const SearchProductContent = () => {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("search");
+  const category = searchParams.get("cat");
 
-    return (
-      <div className="px-4 md:px-8 lg:px-16 xl:px-32">
-        {/* {query.length > 0 ? ( */}
-        <div className="">
-          <Filter />
-          <h1 className="mt-12 text-xl font-semibold">
-            Search results for &quot{query} &quot
-          </h1>
-          <Suspense fallback={<Skeleton />}>
-            <Productlist searchParams={query} category={category} />
-          </Suspense>
-        </div>
-        {/* ) : (
-          <div className=" flex items-center justify-center h-50 text-lg text-gray-500">
-            Product not found
-          </div>
-        )} */}
+  return (
+    <>
+      <h1 className="mt-12 text-xl font-semibold">
+        Search results for "{query}"
+      </h1>
+      <Productlist searchParams={query} category={category} />
+    </>
+  );
+};
+
+const ListingPage = () => {
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32">
+      <div className="">
+        <Filter />
+
+        <Suspense fallback={<Skeleton />}>
+          <SearchProductContent />
+        </Suspense>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default ListingPage;
