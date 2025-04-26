cfd1b455ef67035fef58934c6d7b8ce6584dbd7f
gaurish <33gaurishpatil@gmail.com>
2025-04-26 15:15:13 +0530

    updated homepage

--- src/app/listingPage/page.tsx
+++ src/app/listingPage/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense } from "react";
 
 import Skeleton from "@/components/Skeleton";
 import Productlist from "@/components/Productlist";
@@ -26,7 +26,7 @@ const ListingPage = () =>
         <div className="">
           <Filter />
           <h1 className="mt-12 text-xl font-semibold">
-            Search results for '{query}'
+            Search results for "{query}"
           </h1>
           <Suspense fallback={<Skeleton />}>
             <Productlist searchParams={query} category={category} />
