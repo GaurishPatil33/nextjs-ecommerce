e49083fc0bd691f4452425f60ea9c3f025bf171a
gaurish <33gaurishpatil@gmail.com>
2025-04-26 15:41:59 +0530

    updated homepage

--- src/app/listingPage/page.tsx
+++ src/app/listingPage/page.tsx
@@ -2,38 +2,32 @@
 
 import { useSearchParams } from "next/navigation";
 import React, { Suspense } from "react";
-
 import Skeleton from "@/components/Skeleton";
 import Productlist from "@/components/Productlist";
-import Filter from "@/components/Filter";
 
-const SearchProductContent = () => {
-  const searchParams = useSearchParams();
-  const query = searchParams.get("search");
-  const category = searchParams.get("cat");
+const SearchProductsPage = () => {
+  const router = useSearchParams();
+  const query = router.get("search");
+  const category = router.get("cat");
 
-  return (
-    <>
-      <h1 className="mt-12 text-xl font-semibold">
-        Search results for &quot; {query} &quot;
-      </h1>
-      <Productlist searchParams={query} category={category} />
-    </>
-  );
-};
-
-const ListingPage = () => {
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32">
-      <div className="">
-        <Filter />
-
-        <Suspense fallback={<Skeleton />}>
-          <SearchProductContent />
-        </Suspense>
-      </div>
+      <h1 className="mt-12 text-xl font-semibold">
+        Search results for '{query}'
+      </h1>
+      <Suspense>
+        <Productlist searchParams={query} category={category} />
+      </Suspense>
     </div>
   );
 };
 
-export default ListingPage;
+const DynamicSearchProductsPage = () => {
+  return (
+    <Suspense fallback={<Skeleton />}>
+      <SearchProductsPage />
+    </Suspense>
+  );
+};
+
+export default DynamicSearchProductsPage;
