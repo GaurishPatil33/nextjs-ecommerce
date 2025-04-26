3fb3d983f8b37ef21bbc25d00aff6a07d477b620
gaurish <33gaurishpatil@gmail.com>
2025-04-26 15:43:22 +0530

    updated homepage

--- src/app/listingPage/page.tsx
+++ src/app/listingPage/page.tsx
@@ -4,6 +4,7 @@ import { useSearchParams } from "next/navigation";
 import React, { Suspense } from "react";
 import Skeleton from "@/components/Skeleton";
 import Productlist from "@/components/Productlist";
+import Filter from "@/components/Filter";
 
 const SearchProductsPage = () => {
   const router = useSearchParams();
@@ -12,12 +13,12 @@ const SearchProductsPage = () => {
 
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32">
+      <Filter />
       <h1 className="mt-12 text-xl font-semibold">
-        Search results for '{query}'
+        Search results for {query}
       </h1>
-      <Suspense>
-        <Productlist searchParams={query} category={category} />
-      </Suspense>
+
+      <Productlist searchParams={query} category={category} />
     </div>
   );
 };
